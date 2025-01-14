b42278aa2e4d5fd8e9050b8ef715efd64113b200
Maíra Albuquerque <mairaalbuquerque@fabios-mbp.lan>
2025-01-14 11:36:10 +0000

    ajuste do botão 2

--- app/second.tsx
+++ app/second.tsx
@@ -6,7 +6,7 @@ export default function SecondPage() {
         <View className="bg-blue-100 w-full h-full">
             <Link href="/" asChild>
                 <Pressable className="bg-white m-auto mt-50 p-30 rounded-lg">
-                    <Text className="not-italic font-light text-center text-slate-500 p-20  ">Voltar</Text>
+                    <Text className="not-italic font-light text-center text-slate-500 p-20  ">Voltar!!!</Text>
                 </Pressable>
             </Link>
         </View>    
